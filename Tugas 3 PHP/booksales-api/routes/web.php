bf70c0358fc3b8842e25623096d2edb34d818424
Muhammad Rayyan Ghifari <135932354+Rayyan16ghifari@users.noreply.github.com>
2025-06-04 11:41:13 +0700

    Delete Tugas 3 PHP/booksales-api directory

--- Tugas 3 PHP/booksales-api/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
-use App\Http\Controllers\GenreController;
-
-Route::get('/authors', [AuthorController::class, 'index']);
-Route::get('/genres', [GenreController::class, 'index']);
